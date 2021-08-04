8b400ff86524ce05d34c89b314b43d4d990af7e2
Andrew <atg27@uw.edu>
2021-08-04 08:31:15 -0700

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validates :clickbait
+end
+
+def clickbait 
+    if title
+        strings = ["Won't Believe", "Secret", "Top", "Guess"]
+        title_check = strings.select do |string|
+            title.include?(string)
+        end
+
+        if title_check.empty?
+            errors.add(:title, "must be sufficiently clickbaity")
+        end
+    end
 end
